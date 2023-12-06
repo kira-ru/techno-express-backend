b23346532ba735e752158c424de86959b83c6103
kruban <kruban@digital-spirit.ru>
2023-12-06 15:58:22 +0300

    db connection

---
+++ src/config/database/database.config.ts
@@ -0,0 +1,15 @@
+import {config} from 'dotenv'
+import {Sequelize} from 'sequelize';
+
+config()
+const PORT = process.env.DB_PORT ? +process.env.DB_PORT : 5432;
+
+export const DB = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: PORT,
+  dialect: 'postgres',
+});
+
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import process from 'process'
+import {DB} from './config/database'
+
+const app = express()
+
+try {
+  DB.authenticate()
+      .then(() => {
+        return DB.sync();
+      })
+      .then(() => {
+        app.listen(process.env.PORT, () => {
+          console.log(`Running on port ${process.env.PORT}`)
+        })
+      })
+} catch(e) {
+  console.error(e)
+}
